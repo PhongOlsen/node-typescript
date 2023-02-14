7715e58fa50d6dcf76c912b63dc26fdbce5af6a1
Phong0lsen <p0866017695@gmail.com>
2023-02-14 10:23:10 +0700

    add service chat open ai

--- src/routers/noAuthorizeRouter/index.ts
+++ src/routers/noAuthorizeRouter/index.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express';
 import authRouter from './authRouter';
+import openAIRouter from './openAI';
 
 const noAuthorizeRouter = Router();
 noAuthorizeRouter.use(authRouter);
+noAuthorizeRouter.use(openAIRouter);
 
 export default noAuthorizeRouter;
